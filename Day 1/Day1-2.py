4f003213397a2d68c8129505c8f67c6df0379788
sitiricc <114097346+sitiricc@users.noreply.github.com>
2023-12-03 18:47:22 -0800

    Changed the comment for the code to reflect the part 2 changes.

--- Day 1/Day1-2.py
+++ Day 1/Day1-2.py
@@ -1,9 +1,16 @@
-# Day 1
+# our calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
 
-# The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
+# Equipped with this new information, you now need to find the real first and last digit on each line. For example:
 
+# two1nine
+# eightwothree
+# abcone2threexyz
+# xtwone3four
+# 4nineeightseven2
+# zoneight234
+# 7pqrstsixteen
+# In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
 
-# Consider your entire calibration document. What is the sum of all of the calibration values?
 
 count= 0
 file_path = "Day 1/text_file.txt"
@@ -34,12 +41,13 @@ with open(file_path, 'r') as file:
     file_contents = file.read()
     words= file_contents.split()
     
-    letter_count= 0
+    
     new_digit=0
     first_digit= "0"
     second_digit= "0"
     
     for word in words:
+        integer_total= 0
         # print(word) # prints each word
         for letter in word:
             if letter in numbers_dictionary:
